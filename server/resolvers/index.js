009717e27dd5a3b893ea0a8460f1114d68077f0f
Vamp <25523682+the-vampiire@users.noreply.github.com>
2019-05-24 02:26:52 -0400

    resolvers: scalars

--- server/resolvers/index.js
+++ server/resolvers/index.js
@@ -1,9 +1,11 @@
 const typeResolvers = require("./types");
 const queryResolvers = require("./queries");
+const scalarResolvers = require("./scalars");
 const mutationResolvers = require("./mutations");
 
 module.exports = {
   ...typeResolvers,
   ...queryResolvers,
+  ...scalarResolvers,
   ...mutationResolvers,
 };
---
+++ server/resolvers/scalars.js
@@ -0,0 +1,3 @@
+const { URL, DateTime, EmailAddress } = require("@okgrow/graphql-scalars");
+
+module.exports = { URL, DateTime, EmailAddress };
